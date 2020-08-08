9f07c3852198c384acacd8c6b303f199f844a1fe
MysticalGuest <1317148109@qq.com>
2020-08-08 15:12:35 +0800

    Update Person.java

--- src/main/java/com/initializr/bean/Person.java
+++ src/main/java/com/initializr/bean/Person.java
@@ -1,7 +1,11 @@
 package com.initializr.bean;
 
+import org.hibernate.validator.constraints.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 import java.util.List;
@@ -13,14 +17,31 @@ import java.util.Map;
  * prefix = "person": 配置文件中哪个下面的所有属性进行一一映射
  *
  * 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能
+ * @ConfigurationProperties默认从全局配置文件中获取值
  */
+@PropertySource(value = {"classpath:person.properties"})
 @Component
 @ConfigurationProperties(prefix = "person")
+//@Validated // JSR303数据校验
 public class Person {
 
+    // 另一种方式，Spring的底层注解
+    /**
+     * 类似以前
+     * <bean class="Person">
+     *     <property name="name" value="MysteryGuest"></property>
+     * </bean>
+     * <bean>标签相当于@Compoent
+     * <property>标签中的value相当于@Value
+     * value的参数可以是字面量、${key}从环境变量或配置文件获取值、#{SpEL}
+     */
+//    @Value("${person.name}")
+//    @Email // @value不支持
     private String name;
+//    @Value("#{11*2}")
     private Integer age;
     private Date birth;
+//    @Value("${person.map}")
     private Map<String, Object> map;
     private List<Object> list;
     private Pet pet;
