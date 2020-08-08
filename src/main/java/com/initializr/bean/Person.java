ba0daf19c66f704ae4e55ca9785ea2c411f80757
MysticalGuest <1317148109@qq.com>
2020-08-08 09:31:26 +0800

    Create Person.java

---
+++ src/main/java/com/initializr/bean/Person.java
@@ -0,0 +1,87 @@
+package com.initializr.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 将配置文件中配置的每一个属性的值，映射到这个数组中
+ * @ConfigurationProperties: 告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ * prefix = "person": 配置文件中哪个下面的所有属性进行一一映射
+ *
+ * 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+
+    private String name;
+    private Integer age;
+    private Date birth;
+    private Map<String, Object> map;
+    private List<Object> list;
+    private Pet pet;
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birth=" + birth +
+                ", map=" + map +
+                ", list=" + list +
+                ", pet=" + pet +
+                '}';
+    }
+}
