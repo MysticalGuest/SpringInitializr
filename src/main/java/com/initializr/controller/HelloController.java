0ee9c910034c69e21730aa7ca6a910df0f76b1c8
MysticalGuest <1317148109@qq.com>
2020-08-08 15:12:41 +0800

    Update HelloController.java

--- src/main/java/com/initializr/controller/HelloController.java
+++ src/main/java/com/initializr/controller/HelloController.java
@@ -1,20 +1,27 @@
 package com.initializr.controller;
 
 
+import com.initializr.bean.Person;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Permissions;
 
 //@Controller
 //@ResponseBody
 @RestController // 这个类的所有方法返回的数据直接写给浏览器，（如果是对象转换为JSON数据）
 public class HelloController {
 
+    @Value("${person.name}")
+    private String name;
+
     @RequestMapping()
 //    @ResponseBody
     public String Hello(){
-        return "Hello World!";
+        return "Hello World!" + name;
     }
 
     // RESTAPI的方式
