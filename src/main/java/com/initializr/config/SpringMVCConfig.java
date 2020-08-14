7ad8407aaa4d86e15d024e0564b189875b1ecf10
MysticalGuest <1317148109@qq.com>
2020-08-14 18:11:07 +0800

    Create SpringMVCConfig.java

---
+++ src/main/java/com/initializr/config/SpringMVCConfig.java
@@ -0,0 +1,56 @@
+package com.initializr.config;
+
+import com.initializr.component.LoginHandlerInterceptor;
+import com.initializr.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+// 全面接管SpringMVC
+//@EnableWebMvc
+// 使用WebMvcConfigurerAdapter可以来扩展SpringMVC的功能
+@Configuration
+public class SpringMVCConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // super.addViewControllers(registry);
+        //浏览器发送 /addViewController 请求来到 thymeleaf
+        registry.addViewController("/addViewController").setViewName("thymeleaf");
+    }
+
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean //将组件注册在容器
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            // 注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+//                super.addInterceptors(registry);
+                // "/**"表示任意多层路径下的任意请求
+                // 静态资源；  *.css , *.js
+                // SpringBoot已经做好了静态资源映射
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html", "/", "/login");
+            }
+        };
+        return adapter;
+    }
+
+    // 区域信息解析器组件
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+}
