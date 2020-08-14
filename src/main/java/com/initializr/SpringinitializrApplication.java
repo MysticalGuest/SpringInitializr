f2a62d6aba164050df3c9d81c8f3cd7ccde4b996
MysticalGuest <1317148109@qq.com>
2020-08-14 18:13:28 +0800

    Update SpringinitializrApplication.java

--- src/main/java/com/initializr/SpringinitializrApplication.java
+++ src/main/java/com/initializr/SpringinitializrApplication.java
@@ -2,7 +2,12 @@ package com.initializr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 //@ImportResource(locations = {"classpath:beans.xml"})
 @SpringBootApplication
@@ -12,6 +17,20 @@ public class SpringinitializrApplication {
         System.out.println("Start...");
         SpringApplication.run(SpringinitializrApplication.class, args);
         System.out.println("Success...");
+    }
+
+    // 定制视图解析器
+    @Bean
+    public ViewResolver myViewResolver() {
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver {
+
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return null;
+        }
     }
 
 }
