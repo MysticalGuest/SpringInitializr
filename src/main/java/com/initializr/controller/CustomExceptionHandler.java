b601c9e308ffcfd1407445d23d4c4a7fa4754686
MysticalGuest <1317148109@qq.com>
2020-08-16 13:03:13 +0800

    Create CustomExceptionHandler.java

---
+++ src/main/java/com/initializr/controller/CustomExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.initializr.controller;
+
+import com.initializr.exception.UserNotExistException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+//    1.浏览器和客户端返回都是JSON数据
+    // 只要出现异常，SpringMVC就会调用这个方法，把异常对象传进来
+//    @ResponseBody
+//    @ExceptionHandler(UserNotExistException.class)
+//    public Map<String, Object> handleException(Exception e){
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("code", "user not exist");
+//        map.put("message", e.getMessage());
+//        return map;
+//    }
+
+    @ExceptionHandler(UserNotExistException.class)
+    public String handleException(Exception e, HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        //传入我们自己的错误状态码  4xx 5xx，否则就不会进入定制错误页面的解析流程
+        /**
+         * AbstractErrorController.class中
+         * Integer statusCode = (Integer) request
+         .getAttribute("javax.servlet.error.status_code");
+         */
+        request.setAttribute("javax.servlet.error.status_code", 500);
+        map.put("code", "user not exist");
+        map.put("message", e.getMessage());
+
+        request.setAttribute("myException", map);
+        // 转发到/error
+        return "forward:/error";
+    }
+}
