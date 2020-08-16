32933ecfcd247c1ddb970660f759d667c74b496f
MysticalGuest <1317148109@qq.com>
2020-08-16 13:03:06 +0800

    Create ServletConfig.java

---
+++ src/main/java/com/initializr/config/ServletConfig.java
@@ -0,0 +1,54 @@
+package com.initializr.config;
+
+import com.initializr.filter.CustomFilter;
+import com.initializr.listener.CustomListener;
+import com.initializr.servlet.CustomServlet;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class ServletConfig {
+
+    // 注册三大组件
+    @Bean
+    public ServletRegistrationBean myServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new CustomServlet(),"/myServlet");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        // 设置自己的filter
+        registrationBean.setFilter(new CustomFilter());
+        // 拦截哪些请求
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        ServletListenerRegistrationBean<CustomListener> registrationBean = new ServletListenerRegistrationBean<>(new CustomListener());
+        return registrationBean;
+    }
+
+    // 配置嵌入式的servlet容器
+    @Bean  //一定要将这个定制器加入到容器中
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+
+            //定制嵌入式的Servlet容器相关的规则
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.setPort(8089);
+            }
+        };
+    }
+}
