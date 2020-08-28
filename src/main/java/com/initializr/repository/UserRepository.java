50c11ae5e48f474636170d8213757afa7384fc98
MysticalGuest <1317148109@qq.com>
2020-08-28 18:17:03 +0800

    Create UserRepository.java

---
+++ src/main/java/com/initializr/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.initializr.repository;
+
+import com.initializr.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//继承JpaRepository来完成对数据库的操作
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
