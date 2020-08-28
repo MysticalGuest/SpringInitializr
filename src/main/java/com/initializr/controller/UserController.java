589c07366d2b83763d0a06ba2e844be4f720ac87
MysticalGuest <1317148109@qq.com>
2020-08-28 18:19:02 +0800

    Create UserController.java

---
+++ src/main/java/com/initializr/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.initializr.controller;
+
+import com.initializr.entities.User;
+import com.initializr.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id){
+        User user = userRepository.findOne(id);
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user){
+        // 有自增组件
+        User save = userRepository.save(user);
+        return save;
+    }
+}
